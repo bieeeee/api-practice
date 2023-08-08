92fa59ad76da1ef479227601d79d1ed2d2992218
hanbyeol kwon <janviertrois@naver.com>
2023-08-08 16:26:02 -0400

    Add header, style

--- travel-advisor/src/components/Header/Header.js
+++ travel-advisor/src/components/Header/Header.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import './header.css';
+import { Autocomplete } from '@react-google-maps/api';
+import SearchIcon from '@mui/icons-material/Search';
 
 function Header() {
   return (
-    <div>Header</div>
+    <div className="navbar">
+      <div className="navContainer">
+        <a href="/" className='logo'>Travel Advisor</a>
+        <Autocomplete>
+          <div className='searchContainer'>
+            <SearchIcon />
+            <input placeholder='Search...' />
+          </div>
+        </Autocomplete>
+      </div>
+    </div>
   )
 }
 
