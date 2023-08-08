df781d844221a165c0adda2216a115e70370e5f5
hanbyeol kwon <janviertrois@naver.com>
2023-08-08 17:04:12 -0400

    Add google map

--- travel-advisor/src/components/Map/Map.js
+++ travel-advisor/src/components/Map/Map.js
@@ -1,8 +1,24 @@
-import React from 'react'
+import './map.css';
+import GoogleMapReact from 'google-map-react';
+import { Paper, Typography, useMediaQuery, Rating } from '@mui/material';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 
 function Map() {
+  const isMobile = useMediaQuery('(min-width: 600px');
+  const coordinates = { lat: 0, lng: 0 };
+
   return (
-    <div>Map</div>
+    <div className="mapContainer">
+      <GoogleMapReact
+        bootstrapURLKeys={{ key: process.env.GOOGLE_MAP_API_KEY }}
+        defaultCenter={coordinates}
+        center={coordinates}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+      >
+
+      </GoogleMapReact>
+    </div>
   )
 }
 
