f4ef7714dfc3219cc7ffc033e436bae6036376e5
hanbyeol kwon <janviertrois@naver.com>
2023-08-08 17:29:41 -0400

    Add forms for sort by

--- travel-advisor/src/components/List/List.js
+++ travel-advisor/src/components/List/List.js
@@ -1,8 +1,34 @@
-import React from 'react'
+import "./list.css";
+import { useState } from "react";
+import { CircularProgress, Grid, Typography, InputLabel, FormControl, MenuItem, Select } from "@mui/material";
 
 function List() {
+  const [type, setType] = useState('restaurants');
+  const [rating, setRating] = useState('');
+
+
   return (
-    <div>List</div>
+    <div className="listContainer">
+      <h4>Restaurants, Hotels & Attractions around you</h4>
+      <FormControl className="listForm" variant="standard">
+        <InputLabel id="type">Type</InputLabel>
+        <Select value={type} id="type" onChange={(e) => setType(e.target.value)}>
+          <MenuItem value="restaurants">Restaurants</MenuItem>
+          <MenuItem value="hotels">Hotels</MenuItem>
+          <MenuItem value="attractions">Attractions</MenuItem>
+        </Select>
+      </FormControl>
+      <FormControl className="listForm" variant="standard">
+        <InputLabel id="rating">Rating</InputLabel>
+        <Select value={rating} id="rating" onChange={(e) => setRating(e.target.value)}>
+          <MenuItem value={0}>All</MenuItem>
+          <MenuItem value={3}>Above 3.0</MenuItem>
+          <MenuItem value={4}>Above 4.0</MenuItem>
+          <MenuItem value={4.5}>Above 4.5</MenuItem>
+        </Select>
+      </FormControl>
+
+    </div>
   )
 }
 
