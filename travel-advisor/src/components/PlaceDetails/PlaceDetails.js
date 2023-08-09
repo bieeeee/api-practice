887e00a2479cb2fb3802480db6c7edf37328c7cb
hanbyeol kwon <janviertrois@naver.com>
2023-08-09 11:35:04 -0400

    Add places detail info

--- travel-advisor/src/components/PlaceDetails/PlaceDetails.js
+++ travel-advisor/src/components/PlaceDetails/PlaceDetails.js
@@ -1,8 +1,48 @@
-import React from 'react'
+import "./placeDetails.css";
+import { Box, Typography, Button, Card, CardMedia, CardContent, CardActions, Chip, Rating } from "@mui/material";
+import { Phone, LocationOn } from "@mui/icons-material";
 
-function PlaceDetails() {
+function PlaceDetails({ place }) {
   return (
-    <div>PlaceDetails</div>
+    <Card elevation={6}>
+      <CardMedia
+        style={{ height: 350 }}
+        image={place.photo ? place.photo.images.large.url : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"}
+        title={place.name}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5">{place.name}</Typography>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Price</Typography>
+          <Typography gutterBottom variant="subtitle1">{place.price_level}</Typography>
+        </Box>
+        <Box display="flex" justifyContent="space-between">
+          <Typography variant="subtitle1">Ranking</Typography>
+          <Typography gutterBottom variant="subtitle1">{place.ranking}</Typography>
+        </Box>
+        {place?.cuisine?.map(({ name }) => (
+          <Chip key={name} size="small" label={name} className="chip" />
+        ))}
+        {place?.address && (
+          <Typography gutterBottom variant="subtitle2" color="textSecondary" className="subtitle">
+            <LocationOn /> {place.address}
+          </Typography>
+        )}
+        {place?.phone && (
+          <Typography gutterBottom variant="subtitle2" color="textSecondary" className="spacing">
+            <Phone /> {place.phone}
+          </Typography>
+        )}
+        <CardActions>
+          <Button size="small" color="primary" onClick={() => window.open(place.web_url, "_blank")}>
+            Trip Advisor
+          </Button>
+          <Button size="small" color="primary" onClick={() => window.open(place.website, "_blank")}>
+            Website
+          </Button>
+        </CardActions>
+      </CardContent>
+    </Card>
   )
 }
 
