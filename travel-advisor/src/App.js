9d06b95ad6b5f0cc348c3be1a860b12632bcb71d
hanbyeol kwon <janviertrois@naver.com>
2023-08-09 11:15:21 -0400

    Get current location and set coordinates, pass props to list, map components

--- travel-advisor/src/App.js
+++ travel-advisor/src/App.js
@@ -1,20 +1,43 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { getPlacesData } from './api';
 import { CssBaseline, Grid } from '@mui/material';
 import Header from './components/Header/Header';
 import List from './components/List/List';
 import Map from './components/Map/Map';
 
 const App = () => {
+  const [places, setPlaces] = useState([]);
+  const [coordinates, setCoordinates] = useState({});
+  const [bounds, setBounds] = useState({});
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(({coords: {latitude, longitude}})=>{
+      setCoordinates({lat: latitude, lng: longitude});
+    });
+  }, []);
+
+  useEffect(() => {
+    getPlacesData(bounds.sw, bounds.ne)
+      .then((data) => {
+        setPlaces(data);
+      })
+  }, [coordinates, bounds]);
+
   return (
     <>
       <CssBaseline />
       <Header />
       <Grid container spacing={3} style={{ width: "100%" }}>
         <Grid item xs={12} md={4}>
-          <List />
+          <List places={places} />
         </Grid>
-        <Grid item xs={12} md={8}>
-          <Map />
+        <Grid item xs={12} md={8} style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+          <Map
+            setCoordinates={setCoordinates}
+            setBounds={setBounds}
+            coordinates={coordinates}
+            places={places}
+          />
         </Grid>
       </Grid>
     </>
