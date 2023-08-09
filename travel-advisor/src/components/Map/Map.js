6e3afd327e76f55fd1e6b35887040f09b510578e
hanbyeol kwon <janviertrois@naver.com>
2023-08-09 11:34:14 -0400

    Set cordinate, bounds  onChange, add marker on the map

--- travel-advisor/src/components/Map/Map.js
+++ travel-advisor/src/components/Map/Map.js
@@ -3,9 +3,8 @@ import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery, Rating } from '@mui/material';
 import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
 
-function Map() {
-  const isMobile = useMediaQuery('(min-width: 600px');
-  const coordinates = { lat: 0, lng: 0 };
+function Map({ setCoordinates, setBounds, coordinates, places }) {
+  const isDesktop = useMediaQuery('(min-width: 600px');
 
   return (
     <div className="mapContainer">
@@ -15,8 +14,27 @@ function Map() {
         center={coordinates}
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
+        onChange={(e) => {
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng })
+          setBounds({ sw: e.marginBounds.sw, ne: e.marginBounds.ne })
+        }}
       >
-
+        {places?.map((place, i)=>(
+          <div className="markerContainer" lat={Number(place.latitude)} lng={Number(place.longitude)} key={i}>
+            {
+              !isDesktop ? (
+                <LocationOnOutlinedIcon color="primary" fontSize="large"/>
+              ) : (
+                <Paper elevation={3} className="paper">
+                  <Typography className="typography" variant="subtitle2" gutterBottom>
+                    {place.name}
+                  </Typography>
+                  <img alt={place.name} className="pointer" src={place.photo ? place.photo.images.large.url : "https://www.foodserviceandhospitality.com/wp-content/uploads/2016/09/Restaurant-Placeholder-001.jpg"} />
+                </Paper>
+              )
+            }
+          </div>
+        ))}
       </GoogleMapReact>
     </div>
   )
