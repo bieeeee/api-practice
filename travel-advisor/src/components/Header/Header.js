006fb5401319b7a14a04e9c8f415cca2dbfa1f67
hanbyeol kwon <janviertrois@naver.com>
2023-08-10 12:50:45 -0400

    Add autocomplete

--- travel-advisor/src/components/Header/Header.js
+++ travel-advisor/src/components/Header/Header.js
@@ -1,13 +1,23 @@
 import './header.css';
 import { Autocomplete } from '@react-google-maps/api';
 import SearchIcon from '@mui/icons-material/Search';
+import { useState } from 'react';
 
-function Header() {
+function Header({ setCoordinates }) {
+  const [autocomplete, setAutocomplete] = useState();
+  const onLoad = (autoC) => setAutocomplete(autoC);
+  const onPlaceChanged = () => {
+    console.log(autocomplete.getPlace());
+    const lat = autocomplete.getPlace().geometry.location.lat();
+    const lng = autocomplete.getPlace().geometry.location.lng();
+    setCoordinates({lat, lng});
+  }
+
   return (
     <div className="navbar">
       <div className="navContainer">
         <a href="/" className='logo'>Travel Advisor</a>
-        <Autocomplete>
+        <Autocomplete onLoad={onLoad} onPlaceChanged={onPlaceChanged}>
           <div className='searchContainer'>
             <SearchIcon />
             <input placeholder='Search...' />
