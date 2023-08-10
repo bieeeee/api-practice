69941d27045cb2371de4fc23aa632c85a32d8b5f
hanbyeol kwon <janviertrois@naver.com>
2023-08-10 13:39:35 -0400

    Fetch weather data

--- travel-advisor/src/api/index.js
+++ travel-advisor/src/api/index.js
@@ -4,18 +4,33 @@ export const getPlacesData = async (type, sw, ne) => {
 
   try {
     const { data: { data } } = await axios.get(`https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`
-    , {
-      params: {
-        bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-      },
-      headers: {
-        'X-RapidAPI-Key': '930b5c48bemsh378fdf89f90727ep18039bjsnf83f545d67a2',
-        'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
-      }
-    });
+      , {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+          'X-RapidAPI-Host': 'travel-advisor.p.rapidapi.com'
+        }
+      });
+    return data;
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+export const getWeatherData = async (lat, lng) => {
+  try {
+    const { data } = await axios.get(`https://open-weather13.p.rapidapi.com/city/latlon/${lat}/${lng}`,
+      {
+        headers: {
+          'X-RapidAPI-Key': process.env.RAPID_API_KEY,
+          'X-RapidAPI-Host': 'open-weather13.p.rapidapi.com'
+        }
+      })
     return data;
   } catch (err) {
     console.log(err);
